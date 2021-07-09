59c4352802bb5d729cd5132744b99d6d0fcf92f6
Drew Hartman <drewdavishartman@gmail.com>
2021-07-08 23:25:34 -0400

    adding ad helper file

---
+++ lib/ad_helper.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+class AdHelper {
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-8745617109240050/9580788347';
+    } else if (Platform.isIOS) {
+      return '<YOUR_IOS_BANNER_AD_UNIT_ID>';
+    } else {
+      throw new UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-8745617109240050/8667269722';
+    } else if (Platform.isIOS) {
+      return '<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>';
+    } else {
+      throw new UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static String get rewardedAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-8745617109240050/2273251765';
+    } else if (Platform.isIOS) {
+      return '<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>';
+    } else {
+      throw new UnsupportedError('Unsupported platform');
+    }
+  }
+}
